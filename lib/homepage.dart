2bd5d226f8dba2a22f07d52388bfa6b7cf6fdf55
aishlays1002 <134840571+Aishani312005@users.noreply.github.com>
2025-04-09 11:00:37 +0530

    Added Profile

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -208,7 +208,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => const ProfilePage(),
+                            builder: (context) =>  ProfilePage(),
                           ),
                         );
                       }),
