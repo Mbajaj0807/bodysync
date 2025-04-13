53b850485915eeef050bbe448580d9f5d64aeb18
unknown <anushkaab2005@gmail.com>
2025-04-13 23:05:05 +0530

    Steps

--- lib/progress_tracking.dart
+++ lib/progress_tracking.dart
@@ -1,115 +1,97 @@
-import 'package:fl_chart/fl_chart.dart';
+import 'package:pedometer/pedometer.dart';
+import 'dart:async';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/material.dart';
 import 'Services/weight_graph.dart';
 
-final List<FlSpot> dailyProgress=[
-  FlSpot(0, 5000),
-  FlSpot(1,7000),
-  FlSpot(2, 3000),
-  FlSpot(3,8000),
-  FlSpot(4, 4000),
-  FlSpot(5, 10000),
-  FlSpot(6, 9000),
 
 
-];
-
-class ProgressTracking extends StatelessWidget {
-
-
+class ProgressTracking extends StatefulWidget {
   const ProgressTracking({super.key});
-
-
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: const Text("Progress"),
-          backgroundColor: Color.fromRGBO(137, 108, 254, 1)),
-      backgroundColor: Color.fromRGBO(35, 35, 35, 1),
-
-      body: SingleChildScrollView(
-          padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                const Text(
-                  "Weight Progress",
-                  style: TextStyle(color: Colors.white,fontSize: 18),
-                ),
-                const SizedBox(height:10),
-                SizedBox(height:250,child: WeightGraph()),
-
-                const SizedBox(height:30),
-
-                const Text(
-                  "Daily Steps",
-                  style: TextStyle(color: Colors.white,fontSize: 18),
-                ),
-                const SizedBox(height:10),
-                SizedBox(
-                  height: 250,
-                  child: DailyProgressChart(data: dailyProgress),
-                ),
-              ],
-            ),
-
-        ),
-
-      );
-  }
+  State<ProgressTracking> createState() => _ProgressTrackingState();
 }
 
 
+class _ProgressTrackingState extends State<ProgressTracking> {
+  late Stream<StepCount> _stepCountStream;
+  int _steps=0;
 
-class DailyProgressChart extends StatelessWidget{
-  final List<FlSpot> data;
-  const DailyProgressChart({super.key,required this.data});
+  @override
+  void initState(){
+    super.initState();
+    _requestPermission();
+  }
+
+  Future<void> _requestPermission() async{
+    PermissionStatus status=await Permission.activityRecognition.request();
+
+    if(status.isGranted){
+      _startListening();
+    }else{
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: const Text("Permission to track steps."),
+        action: SnackBarAction(label: "Settings", onPressed: openAppSettings),
+      ));
+    }
+  }
+
+  void _startListening(){
+    _stepCountStream=Pedometer.stepCountStream;
+    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+  }
+
+  void onStepCount(StepCount event){
+    setState(() {
+      _steps=event.steps;
+    });
+
+  }
+
+  void onStepCountError(error){
+    print("Step count error: $error");
+
+  }
 
   @override
   Widget build(BuildContext context){
-    return LineChart(
-      LineChartData(
-        gridData: FlGridData(show:true),
-        titlesData: FlTitlesData(
-            leftTitles: AxisTitles(
-              sideTitles: SideTitles(showTitles: true)
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Progress"),
+        backgroundColor: Color.fromRGBO(137, 108, 254, 1),
+      ),
+      backgroundColor: const Color.fromRGBO(35, 35, 35, 1),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            const Text("Weight Progress",style: TextStyle(fontSize: 18,color: Colors.white),
             ),
-          bottomTitles:
-            AxisTitles(
-              sideTitles: SideTitles(
-                showTitles: true,
-                getTitlesWidget: (value, _) => Text(
-                  ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'][value.toInt()],
-                  style: TextStyle(fontSize: 10),
-                ),
-                interval: 1,
-              )
+            const SizedBox(height:10),
+            SizedBox(height:250,child:WeightGraph()),
+
+            const SizedBox(height: 30),
+            const Text("Live Step Count",style: TextStyle(fontSize: 18,color: Colors.white),),
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white12,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Text(
+                'Steps Taken Today: $_steps',
+                style: const TextStyle(fontSize: 20, color: Colors.white),
+              ),
             )
-
-        ),
-          borderData: FlBorderData(show: true),
-        lineBarsData: [
-          LineChartBarData(
-            spots:data,
-            isCurved: true,
-            color:Colors.blue,
-            barWidth: 3,
-            dotData: FlDotData(show:true),
-          )
-        ]
-
+          ],
+        )
       )
-
     );
-
-
-
   }
-
-
-
-
 }
+
+
+
+
 
 
 
